cfb87f0881e67e77d30635c638fdd81731f587ee
EliteRodrigo <f.rui@netcabo.pt>
2024-03-02 14:33:27 +0000

    Corrected tangram composition with exact values for all translations

--- tp2/MyTangram.js
+++ tp2/MyTangram.js
@@ -1,4 +1,4 @@
-import {CGFobject} from '../lib/CGF.js';
+import { CGFobject } from '../lib/CGF.js';
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
@@ -10,67 +10,66 @@ import { MyTriangleSmall } from "./MyTriangleSmall.js";
  * @param scene - Reference to MyScene object
  */
 export class MyTangram extends CGFobject {
-	constructor(scene) {
-		super(scene);
+    constructor(scene) {
+        super(scene);
         this.diamond = new MyDiamond(scene);
         this.triangle = new MyTriangle(scene);
         this.parallelogram = new MyParallelogram(scene);
         this.triangleSmall = new MyTriangleSmall(scene);
         this.triangleBig = new MyTriangleBig(scene);
-	}
-	
+    }
+
     display() {
         var translateDiamond = [
             1, 0, 0, 0,
             0, 1, 0, 0,
             0, 0, 1, 0,
-            Math.SQRT2-1, Math.SQRT2, 0, 1
+            Math.SQRT2 - 1, Math.SQRT2, 0, 1
         ];
-    
         // ---- BEGIN Primitive drawing section
         this.scene.pushMatrix()
         this.scene.multMatrix(translateDiamond);
         this.diamond.display();
         this.scene.popMatrix()
-    
+
         this.scene.pushMatrix()
-        this.scene.translate(Math.SQRT2,0,0)
-        this.scene.rotate(Math.PI/180*225, 0, 0, 1)
+        this.scene.translate(Math.SQRT2, 0, 0)
+        this.scene.rotate(Math.PI / 180 * 225, 0, 0, 1)
         this.triangle.display();
         this.scene.popMatrix()
-    
+
         this.scene.pushMatrix()
-        this.scene.translate(Math.SQRT2,-Math.SQRT2,0)
-        this.scene.rotate(Math.PI/180*45, 0, 0, 1)
+        this.scene.translate(Math.SQRT2, -Math.SQRT2, 0)
+        this.scene.rotate(Math.PI / 180 * 45, 0, 0, 1)
         this.triangleBig.display();
         this.scene.popMatrix()
-    
+
         this.scene.pushMatrix()
-        this.scene.translate(-Math.SQRT2,-Math.SQRT2,0)
-        this.scene.rotate(Math.PI/180*225, 0, 0, 1)
+        this.scene.translate(-Math.SQRT2, -Math.SQRT2, 0)
+        this.scene.rotate(Math.PI / 180 * 225, 0, 0, 1)
         this.triangleBig.display();
         this.scene.popMatrix()
-    
+
         this.scene.pushMatrix()
-        this.scene.translate(0.12,2.12,0)
-        this.scene.rotate(Math.PI/180*45, 0, 0, 1)
+        this.scene.translate(3 / 2 * Math.SQRT2 - 2, Math.SQRT2 / 2 + Math.SQRT2, 0)
+        this.scene.rotate(Math.PI / 180 * 45, 0, 0, 1)
         this.triangleSmall.display();
         this.scene.popMatrix()
-    
-    
+
+
         this.scene.pushMatrix()
-        this.scene.translate(Math.SQRT2/2,-3.535,0)
-        this.scene.rotate(Math.PI/180*135, 0, 0, 1)
+        this.scene.translate(Math.SQRT2 / 2, -Math.SQRT2 / 2 - Math.sqrt(8), 0)
+        this.scene.rotate(Math.PI / 180 * 135, 0, 0, 1)
         this.triangleSmall.display();
         this.scene.popMatrix()
-    
+
         this.scene.pushMatrix()
-        this.scene.translate(-4.2425,-4.2425,0)
-        this.scene.rotate(Math.PI/180*45, 0, 0, 1)
+        this.scene.translate(-3 / 2 * Math.sqrt(8), -3 / 2 * Math.sqrt(8), 0)
+        this.scene.rotate(Math.PI / 180 * 45, 0, 0, 1)
         this.scene.rotate(Math.PI, 1, 0, 0)
         this.parallelogram.display();
         this.scene.popMatrix()
-    
+
         // ---- END Primitive drawing section
     }
 }
--- tp2/README.md
+++ tp2/README.md
@@ -4,7 +4,7 @@
 
 ## TP 2 Notes
 
-- In the first exercise we created a simple tangram by applying transformations (rotations and translations) to the elements we have used in the last practical class. Then, we created a class named MyTangram, to tidy things up. This way, our code was easier to understand and work with. 
+- In the first exercise we created a simple tangram by applying transformations (rotations and translations) to the elements we have used in the last practical class. We calculated the exact values for the translations. Then, we created a class named MyTangram, to tidy things up. This way, our code was easier to understand and work with. 
 
 ![Screenshot 1](tp2/screenshots/cg-t04g11-tp2-1.png) 
 
